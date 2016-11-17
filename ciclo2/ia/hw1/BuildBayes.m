391df9e8d0286e87315331795df6c3bfce7eceb3
pmendozav <pmendozavx@gmail.com>
2016-11-17 10:53:08 -0500

    Update BuildBayes.m

--- ciclo2/ia/hw1/BuildBayes.m
+++ ciclo2/ia/hw1/BuildBayes.m
@@ -6,10 +6,8 @@ function out = BuildBayes(X, C)
     for i=1:dim
         xi = X(:,i);
         tags_xi{i} = unique(xi, 'stable');
-
         for j=1:size(tag_c,1)
             oc_cj = strcmp(C, tag_c(j));
-
             for k=1:size(tags_xi{i},1)
                 tag = tags_xi{i}{k};
                 pxc = sum(and(strcmp(xi, tag), oc_cj));
